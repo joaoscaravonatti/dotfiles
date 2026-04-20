eb0f55c7cc674749fe6e4f4c14a94c060aac5868
João Scaravonatti <jvscaravonatti@gmail.com>
2026-04-20 09:53:46 -0300

    Add files via upload

---
+++ hypr/awww.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+WALLPAPERS="/home/joao/Pictures/Wallpapers"
+CURRENT_WALLPAPER=0
+
+while true; do	
+  FILES=($(ls "$WALLPAPERS"))
+  LENGTH=${#FILES[@]}
+  let MAX_INDEX=LENGTH-1
+  IMAGE=${FILES[CURRENT_WALLPAPER]}
+  IMG_PATH="$WALLPAPERS/$IMAGE"
+
+  awww img --transition-duration 1 "$IMG_PATH"
+
+  if [[ "$CURRENT_WALLPAPER" -lt "$MAX_INDEX" ]]; then
+    let CURRENT_WALLPAPER=CURRENT_WALLPAPER+1
+  else
+    CURRENT_WALLPAPER=0
+  fi
+
+  sleep 60
+done
