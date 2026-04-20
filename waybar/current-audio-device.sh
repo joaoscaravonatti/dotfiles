6f52ef7ee55f946debd8058b39e1fd39b26e831a
João Scaravonatti <jvscaravonatti@gmail.com>
2026-04-20 09:54:43 -0300

    Add files via upload

--- waybar/current-audio-device.sh
+++ waybar/current-audio-device.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 declare -A names=(
-  ["alsa_output.pci-0000_00_1f.3.analog-stereo"]="󰽟 Interno"
-  ["alsa_output.usb-GeneralPlus_USB_Audio_Device-00.analog-stereo"]="󰋎 HyperX"
-  ["alsa_output.usb---_KTMicro_--_Audio_hs_2.0_headset_2024-05-21-0000-0000-0000-00.analog-stereo"]="󰋋 KZ"
+  ["alsa_output.pci-0000_00_1f.3.analog-stereo"]="󰽟"
+  ["alsa_output.usb-GeneralPlus_USB_Audio_Device-00.analog-stereo"]="󰋎"
+  ["alsa_output.usb---_KTMicro_--_Audio_hs_2.0_headset_2024-05-21-0000-0000-0000-00.analog-stereo"]="󰋋"
 )
 
 current=$(pactl get-default-sink)
-echo "${names[$current]:-🎧 Desconhecido}"
+echo "${names[$current]:-🎧}"
